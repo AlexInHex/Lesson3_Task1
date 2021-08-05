eff08b8ffda6e85020d337d8a6135a3c5aaecabc
PC <Squiblawarcurse@gmail.com>
2021-08-05 20:21:46 +0300

    Добавьте файлы проекта.

---
+++ Lesson2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Lesson2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[20];
+            Random random = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next();
+                Console.WriteLine(array[i]);
+
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] < array[i+1])
+                {
+                    Console.WriteLine(array[i+1]);
+                }
+            }
+        }
+    }
+}
